332fc2c964dcee0495c90dc33e747adaaf4e58cb
Jonathan Hartman <jhartman86@gmail.com>
2013-06-26 15:38:53 -0600

    Package version increment

--- web/packages/clinica/controller.php
+++ web/packages/clinica/controller.php
@@ -4,7 +4,7 @@
 	
 	    protected $pkgHandle 			= 'clinica';
 	    protected $appVersionRequired 	= '5.6.1';
-	    protected $pkgVersion 			= '0.59';
+	    protected $pkgVersion 			= '0.61';
 	
 		
 		/**
