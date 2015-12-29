196c2612be06aabf14793b00bf0b948209584853
Jonathan Hartman <jhartman86@gmail.com>
2015-12-29 13:28:08 -0800

    Removed unused cruft from gulp build file; also removed bower dependency management because theres not really a way to use it for Clinica without a significant refactor.

--- web/packages/clinica/gulp.js
+++ web/packages/clinica/gulp.js
@@ -28,29 +28,6 @@ module.exports = function( gulp ){
                 ie8: _pathTo('css/src/ie8.scss')
             }
         };
-        //sources = {
-        //    scss: {
-        //        core: _pathTo('css/src/core.scss'),
-        //        app: _pathTo('css/src/app.scss')
-        //    },
-        //    js: {
-        //        core: [
-        //            _pathTo('bower_components/fastclick/lib/fastclick.js'),
-        //            _pathTo('bower_components/angular/angular.js'),
-        //            _pathTo('bower_components/angular-sanitize/angular-sanitize.js'),
-        //            _pathTo('bower_components/gsap/src/uncompressed/TweenMax.js'),
-        //            _pathTo('bower_components/gsap/src/uncompressed/plugins/ScrollToPlugin.js'),
-        //            _pathTo('bower_components/imagesloaded/imagesloaded.pkgd.js'),
-        //            _pathTo('bower_components/masonry/dist/masonry.pkgd.js'),
-        //            _pathTo('bower_components/svg.js/dist/svg.min.js'),
-        //            _pathTo('bower_components/moment/moment.js'),
-        //            _pathTo('js/3rd_party/*.js')
-        //        ],
-        //        app: [
-        //            _pathTo('js/src/**/*.js')
-        //        ]
-        //    }
-        //};
 
     /**
      * Sass compilation
@@ -67,39 +44,7 @@ module.exports = function( gulp ){
                 this.emit('end');
             })
             .pipe(gulp.dest(_pathTo('css/')));
-        //return gulp.src(files)
-        //    .pipe(sass({compass:true, style:(_style || 'nested')}))
-        //    .on('error', function( err ){
-        //        utils.log(utils.colors.red(err.toString()));
-        //        this.emit('end');
-        //    })
-        //    .pipe(gulp.dest(_pathTo('css/')));
     }
-
-    /**
-     * Javascript builds (concat, optionally minify)
-     * @param files
-     * @param fileName
-     * @param minify
-     * @returns {*|pipe|pipe}
-     */
-    //function runJs( files, fileName, minify ){
-    //    return gulp.src(files)
-    //        .pipe(concat(fileName))
-    //        .pipe(minify === true ? uglify() : utils.noop())
-    //        .pipe(gulp.dest(_pathTo('js/')));
-    //}
-
-    /**
-     * Run JSHint
-     * @param files
-     * @returns {*|pipe|pipe}
-     */
-    //function runJsHint( files ){
-    //    return gulp.src(files)
-    //        .pipe(jshint(_pathTo('.jshintrc')))
-    //        .pipe(jshint.reporter('jshint-stylish'));
-    //}
 
     /** Register individual tasks */
     //gulp.task(_taskName('jshint'), function(){ return runJsHint(sources.js.app); });
@@ -109,27 +54,19 @@ module.exports = function( gulp ){
     gulp.task(_taskName('sass:app:prod'), function(){ return runSass(sources.scss.app, 'compressed'); });
     gulp.task(_taskName('sass:ie8:dev'), function(){ return runSass(sources.scss.ie8); });
     gulp.task(_taskName('sass:ie8:prod'), function(){ return runSass(sources.scss.ie8, 'compressed'); });
-    //gulp.task(_taskName('js:core:dev'), function(){ return runJs(sources.js.core, 'core.js', false) });
-    //gulp.task(_taskName('js:core:prod'), function(){ return runJs(sources.js.core, 'core.js', true) });
-    //gulp.task(_taskName('js:app:dev'), [_taskName('jshint')], function(){ return runJs(sources.js.app, 'app.js', false) });
-    //gulp.task(_taskName('js:app:prod'), [_taskName('jshint')], function(){ return runJs(sources.js.app, 'app.js', true) });
 
     /** Run all dev tasks */
     gulp.task(_taskName('build:dev'), [
         _taskName('sass:dashboard:dev'),
         _taskName('sass:app:dev'),
-        _taskName('sass:ie8:dev'),
-        //_taskName('js:core:dev'),
-        //_taskName('js:app:dev')
+        _taskName('sass:ie8:dev')
     ], function(){ utils.log(utils.colors.bgGreen('Dev build OK')); });
 
     /** Run all prod tasks */
     gulp.task(_taskName('build:prod'), [
         _taskName('sass:dashboard:prod'),
         _taskName('sass:app:prod'),
-        _taskName('sass:ie8:prod'),
-        //_taskName('js:core:prod'),
-        //_taskName('js:app:prod')
+        _taskName('sass:ie8:prod')
     ], function(){ utils.log(utils.colors.bgGreen('Prod build OK')); });
 
     /** Watches */
@@ -137,7 +74,6 @@ module.exports = function( gulp ){
         gulp.watch(_pathTo('css/src/clinica.app.scss'), {interval:1000}, [_taskName('sass:app:dev')]);
         gulp.watch(_pathTo('css/src/clinica.dashboard.scss'), {interval:1000}, [_taskName('sass:dashboard:dev')]);
         gulp.watch(_pathTo('css/src/ie8.scss'), {interval:1000}, [_taskName('sass:ie8:dev')]);
-        //gulp.watch(_pathTo('js/src/**/*.js'), {interval:1000}, [_taskName('js:app:dev')]);
     });
 
 };
